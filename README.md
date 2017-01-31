ee33a31696d8cead7b3b69a8d60616d1dc3c4f7e
Alec Gibson <aleckgibson@googlemail.com>
2017-01-31 09:50:58 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Bulk Tag Converter Tool
+
+This tool is designed to convert taxon tagging spreadsheets to a format
+accepted by [Content Tagger|https://github.com/alphagov/content-tagger].
+
+## Preparing the spreadsheet
+
+The tool reads spreadsheets from the internet, like Google Sheets. Prepare
+a Google sheet for conversion by going to File > Publish to the web...  
+Then choose the sheet to publish and publish it as Tab-separated values (.tsv)
+
+## Using the tool
+
+Make sure the tool is executable by running `chmod +x bulk_tag_converter.rb`
+
+Execute the script with
+`./bulk_tag_converter.rb 'https://docs.google.com/my-tsv-spreadsheet'
+'output.csv'`
+
+The first argument should be the URL to the TSV published spreadsheet, and
+the second argument should be the path to the output CSV file.
---
+++ bulk_tag_converter.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'csv'
+
+SLUG_MATCHER = /gov.uk(?<slug>.+)/
+UUID_MATCHER = /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
+TAG_COLUMNS_START = 5 # Column number of the first tag entry in a row
+NUMBER_OF_TAGS = 8 # Number of tag entries on a row
+TAG_COLUMNS_END = TAG_COLUMNS_START + NUMBER_OF_TAGS - 1
+TAG_ID_OFFSET = 9 # Number added to a tag column number to get the column number of its ID
+LINK_TYPE = 'taxons'
+URL_COLUMN = 1
+
+def validate_arguments
+  throw "Expected 2 arguments. Please call: ./bulk_tag_converted <tsv_url> <output_file>" unless ARGV.length == 2
+end
+
+def get_spreadsheet
+  uri = URI(ARGV[0])
+  response = Net::HTTP.get_response(uri)
+  throw "#{response.code}: #{response.message}" unless response.code == '200'
+  puts "Successfully fetched spreadsheet"
+  response.body
+end
+
+def to_rows(spreadsheet)
+  Enumerator.new do |r|
+    row_number = 1 # Start at 1 because of headers
+    CSV.parse(spreadsheet, col_sep: "\t", headers: true) do |row|
+      row_number += 1
+      TAG_COLUMNS_START.upto(TAG_COLUMNS_END) do |tag_column|
+        begin
+          tag_id_column = tag_column + TAG_ID_OFFSET
+          tag_number = tag_column - TAG_COLUMNS_START + 1
+
+          next if row[tag_column].nil? || row[tag_column].empty?
+          raise "Empty URL" if row[URL_COLUMN].nil? || row[URL_COLUMN].empty?
+          raise "No Tag#{tag_number} ID" if row[tag_id_column].nil? || row[tag_id_column].empty?
+          raise "Invalid Tag#{tag_number} ID" unless UUID_MATCHER =~ row[tag_id_column]
+
+          r << {
+            content_base_path: row[URL_COLUMN].match(SLUG_MATCHER)[:slug],
+            link_title: row[tag_column],
+            link_content_id: row[tag_id_column],
+            link_type: LINK_TYPE,
+          }
+        rescue Exception => e
+          puts "ERROR on line #{row_number}: #{e.message}"
+        end
+      end
+    end
+  end
+end
+
+def write_to_csv(rows)
+  puts "Writing rows to '#{ARGV[1]}'"
+  CSV.open(ARGV[1], "wb") do |csv|
+    csv << ['content_base_path', 'link_title', 'link_content_id', 'link_type']
+    rows.each do |row|
+      csv << [row[:content_base_path], row[:link_title], row[:link_content_id], row[:link_type]]
+    end
+  end
+  puts "FINISHED"
+end
+
+validate_arguments
+spreadsheet = get_spreadsheet
+rows = to_rows spreadsheet
+write_to_csv rows
